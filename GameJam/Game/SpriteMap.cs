4fedd98a2c9e41c10748f617a9286471eb45a67d
sam <swzwij@gmail.com>
2022-05-13 10:23:06 +0200

    changed / added sprites

--- GameJam/Game/SpriteMap.cs
+++ GameJam/Game/SpriteMap.cs
@@ -16,6 +16,7 @@ namespace GameJam.Game
             tileMap.Add('.', new Rectangle(23, 75, 16, 16));
             tileMap.Add('D', new Rectangle(2, 75, 16, 16));
             tileMap.Add('!', new Rectangle(66, 75, 16, 16));
+            tileMap.Add('*', new Rectangle(87, 75, 16, 16));
 
             playerAnimation = new Rectangle[]
                 {
